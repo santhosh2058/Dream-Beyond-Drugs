c7577a57e4948abe320f4531859205d30e112f89
sai priya <saipriya901041@gmail.com>
2024-06-18 04:55:02 +0530

    added layout pages and updated App.css

---
+++ frontend/src/components/layout/Faqs.js
@@ -0,0 +1,148 @@
+import React from 'react';
+
+
+class Faqs extends React.Component {
+  render() {
+    return (
+      <div className="faq">
+        <div className="container text-center my-5">
+          <h2><strong>Frequently Asked Questions</strong></h2>
+          <div class="accordion accordion-flush my-5" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne" style={{backgroundColor:"#526D82",fontSize:'1.5rem',fontSize:'1.5rem'}}>
+                  How do I start the process of quitting addiction?
+                </button>
+              </h2>
+              <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  To begin quitting any addiction, it's important to approach the process with determination and a plan. Here are some steps you can take to start your journey towards recovery:
+                  <ul >
+                    <li type='1'>
+                      Acknowledge the problem.
+                    </li>
+                    <li type='1'>
+                      Seek support from friends, family, or support groups.
+                    </li>
+                    <li type='1'>
+                      Set clear goals and establish a quit date.
+                    </li>
+                    <li type='1'>
+                    Remove triggers and temptations.
+                    </li>
+                    <li type='1'>
+                    Find healthy alternatives to replace addictive behaviors.
+                    </li>
+                    <li type='1'>
+                    Practice self-care and develop coping mechanisms.
+                    </li>
+                    <li type='1'>
+                    Celebrate milestones along the way.
+                    </li>
+                  </ul>
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  I am smoking for last three years continuously is it late to start quitting it now?
+                </button>
+              </h2>
+              <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  No, it is never too late to start quitting smoking. Regardless of how long you have been smoking, quitting at any point can have significant health benefits. The sooner you quit, the better, as it reduces the risk of developing smoking-related diseases and improves overall health. Even after smoking for three years, quitting now can still greatly improve your well-being and reduce the potential long-term effects of smoking. Seek support, develop a plan, and take the first step towards quitting smoking for a healthier future.
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  I regret after I get back to my senses after having drugs, I want to quit but I can't, what can I do?
+                </button>
+              </h2>
+              <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  If you find yourself regretting drug use and struggling to quit, it's important to acknowledge the problem and seek support from trusted individuals or support groups. Consulting with a healthcare professional can provide guidance and treatment options tailored to your needs. Exploring rehabilitation programs, therapy, and counseling can help address the underlying factors contributing to your drug use. Avoiding triggers, considering therapy or counseling, and taking recovery one day at a time are important steps. Remember, reaching out to local resources for additional assistance and guidance can provide the support you need on your journey to recovery.
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  I am not able to take my mind away from porn whole day, what can I do?
+                </button>
+              </h2>
+              <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  If you're struggling to take your mind away from porn throughout the day, there are steps you can take. Recognize the issue, understand triggers and patterns, create a supportive environment by using content filters and blockers, find healthy alternatives to occupy your mind, seek professional help from a therapist or counselor specializing in addiction, build a support network of trusted individuals, and prioritize self-care to improve overall well-being. Breaking free from a porn addiction takes time and effort, so be patient, celebrate small victories, and seek support when needed.
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFifth" aria-expanded="false" aria-controls="flush-collapseFifth" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  How can I reduce my screen time and manage mobile addiction?
+                </button>
+              </h2>
+              <div id="flush-collapseFifth" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  Set limits on phone usage, create phone-free zones or times, engage in alternative activities, and seek support from family or friends.
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSixth" aria-expanded="false" aria-controls="flush-collapseSixth" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  What are the potential consequences of excessive alcohol consumption?
+                </button>
+              </h2>
+              <div id="flush-collapseSixth" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  Consequences may include liver damage, impaired judgment, addiction, health issues, relationship problems, and increased risk of accidents.
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeventh" aria-expanded="false" aria-controls="flush-collapseSeventh" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  What are the available treatment options for alcohol addiction?
+                </button>
+              </h2>
+              <div id="flush-collapseSeventh" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  Treatment options may include detoxification, counseling, support groups (e.g., Alcoholics Anonymous), and therapy to address underlying issues.
+                  
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button class="accordion-button collapsed text-left" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEigth" aria-expanded="false" aria-controls="flush-collapseEigth" style={{backgroundColor:"#526D82",fontSize:'1.5rem'}}>
+                  Are there any support groups or resources specifically for mobile addiction, alcohol addiction, porn addiction, or drug addiction?
+                </button>
+              </h2>
+              <div id="flush-collapseEigth" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                <div class="accordion-body text-left" style={{fontSize:'1.2rem' }}>
+                  Yes, support groups such as Smartphone Addiction Anonymous, Alcoholics Anonymous, Sex Addicts Anonymous, and Narcotics Anonymous can provide guidance and support.
+                  
+                </div>
+              </div>
+            </div>
+            
+          </div>
+          
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default Faqs;
